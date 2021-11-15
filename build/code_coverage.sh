d22d35a74f56e16dd5dbafc1c8d731ca61d42c0d
allen lian <allen.lian@outlook.com>
2021-11-15 21:41:43 +0800

    add if for error

--- build/code_coverage.sh
+++ build/code_coverage.sh
@@ -1,12 +1,18 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-# unit test
+# switch to current directory
+cd $(dirname $0)/..
+
+# run unit test
 dotnet test --collect "XPlat Code Coverage" -v m
 
 # generate coverage report
-for test_dir in $(find . -type d -name "TestResults")
-do
-    all_tests=$(ls -c $test_dir)
-    lastest_test=${all_tests:0:36}
-    reportgenerator -reports:"$test_dir/$lastest_test/coverage.cobertura.xml" -targetdir:"$test_dir/coveragereport" -reporttypes:Html    
-done
+if [ $? -eq 0 ]
+then
+    for test_dir in $(find . -type d -name "TestResults")
+    do
+        all_tests=$(ls -c $test_dir)
+        lastest_test=${all_tests:0:36}
+        reportgenerator -reports:"$test_dir/$lastest_test/coverage.cobertura.xml" -targetdir:"$test_dir/coveragereport" -reporttypes:Html    
+    done
+fi
