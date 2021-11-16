75e2db6c02bfc6945427eeba4cb3f5bc49f19a36
廉强 <allen.lian@outlook.com>
2021-11-16 10:43:11 +0800

    fix code_coverage.ps1

--- build/code_coverage.ps1
+++ build/code_coverage.ps1
@@ -5,19 +5,18 @@ Set-Location -Path $PSScriptRoot/..
 # run unit test
 dotnet test --collect "XPlat Code Coverage" -v m
 
-# update code coverage
-Get-ChildItem -Path . -Recurse -Include "TestResults" -Directory | ForEach-Object {
-    $testFolderPath = $_.FullName
-    Write-Output "root => $testFolderPath"
-    Get-ChildItem -Path $testFolderPath -Directory | Sort-Object -Descending -Property LastWriteTime | ForEach-Object {
-        $lastest = $_.Name
-        if ($lastest.Length -eq 36) {
-            Write-Output "$lastest"
-            reportgenerator -reports:"$testFolderPath/$lastest/coverage.cobertura.xml" -targetdir:"$testFolderPath/coveragereport" -reporttypes:Html
-            break
-        }
-        else {
-            Write-Output "NOT => $lastest"
+# generate coverage report
+if ($? -ne 0) {
+    Get-ChildItem -Path . -Recurse -Include "TestResults" -Directory | ForEach-Object {
+        $testFolderPath = $_.FullName
+        Write-Output "test folder => $testFolderPath"
+        Get-ChildItem -Path $testFolderPath -Directory | Sort-Object -Descending -Property LastWriteTime | ForEach-Object {
+            $lastest = $_.Name
+            if ($lastest.Length -eq 36) {
+                Write-Output "lastest => $lastest"
+                reportgenerator -reports:"$testFolderPath/$lastest/coverage.cobertura.xml" -targetdir:"$testFolderPath/coveragereport" -reporttypes:Html
+                break
+            }
         }
     }
 }
